94c482e68d971b4b938322b3c936ba7fb1723536
lucccimane <lucio.guido@icloud.com>
2023-11-09 10:53:44 -0500

    Using Swift's Date Types

--- Better Rest/ContentView.swift
+++ Better Rest/ContentView.swift
@@ -16,21 +16,27 @@ struct ContentView: View {
         VStack {
             // modifier labelshidden allows Voice Over to give helpful description
             // Optional init displayComponents cleanly formats Dates and Times
+            // iOS will automatically format date based on local conventions when given in Text( format: .dateTime)
+            Text(Date.now, format: .dateTime.day().month().year())
             DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                 .labelsHidden()
-            
-            // Single ended ranges possible using ellipse within in:
-            DatePicker("Please enter a date", selection: $wakeUp, in: Date.now..., displayedComponents: .date)
-                .labelsHidden()
+
         }
-
-    }
-
-    func exampleDates() {
-        let tomorrow = Date.now.addingTimeInterval(86400)
-        let range = Date.now...tomorrow
     }
     
+    func exampleDates() -> Date {
+        var components = DateComponents()
+        components.hour = 8
+        components.minute = 0
+        let date = Calendar.current.date(from: components) ?? .now
+        return date
+    }
+    
+    func exampleDates2() {
+        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
+        let hour = components.hour ?? 0
+        let minutes = components.minute ?? 0
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
